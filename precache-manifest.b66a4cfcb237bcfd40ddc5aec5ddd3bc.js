bbcacffd9618d0dfb14f04db3aac26e203d3d57b
Lucas Gutierres Gomes <lucas.gutierres.gome@gmail.com>
2019-10-20 01:40:30 -0300

    Updates

---
+++ precache-manifest.b66a4cfcb237bcfd40ddc5aec5ddd3bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "216ef423285f40d33b467510e1956559",
+    "url": "/song-list/index.html"
+  },
+  {
+    "revision": "fd54ba0682244c724aba",
+    "url": "/song-list/static/css/main.35b7633c.chunk.css"
+  },
+  {
+    "revision": "4724ddc4509134e5ebf1",
+    "url": "/song-list/static/js/2.395e6f85.chunk.js"
+  },
+  {
+    "revision": "fd54ba0682244c724aba",
+    "url": "/song-list/static/js/main.af7ea6db.chunk.js"
+  },
+  {
+    "revision": "b4888483e97c87c03fa1",
+    "url": "/song-list/static/js/runtime-main.05d0217a.js"
+  }
+]);
